0ddcc5ecaf53f414b08f52092577068b3ab3204e
Пользователь <eldar.marajapov@gmail.com>
2022-06-13 17:16:15 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+
+function App(){
+    
+        return (
+        <div class="background">
+            <video width="320" height="240" src ="https://assets.mixkit.co/videos/preview/mixkit-blurred-abstract-cars-lights-at-night-with-bokeh-effect-30-large.mp4" autoplay loop muted>
+            Your browser does not support the video tag.
+            </video>
+      
+            <div class="glass">
+                Glass dsfdsf
+            </div>
+      
+        </div>
+    )
+}
+    
+export default App; 
